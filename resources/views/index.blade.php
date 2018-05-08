52e9224b46c391520217f4640e6c8a67201eacc7
ealborn <emma.alborn@gmail.com>
2018-05-08 22:58:59 +0200

    test with local storage btn click

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,22 @@
 
 @section('content')
   <h1>HEJHEJHEJ</h1>
+  <h2>emma testar</h2>
+  <p><button onclick="clickCounter()" type="button">Click me!</button></p>
+  <div id="result"></div>
+  <p>Click the button to see the counter increase.</p>
+  <script>
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+</script>
 @stop
