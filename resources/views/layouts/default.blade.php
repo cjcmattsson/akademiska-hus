5d54346e53d51f14ccfb40328073e4e889ff31fc
cjcmattsson <cjcmattsson@gmail.com>
2018-05-29 16:47:05 +0200

    Added a component for all head style SO AS TO NOT REPEAT OUR CODE. DRY and such

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,26 +5,7 @@ use Illuminate\Support\Facades\Auth;
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Ett enklare och mer mobilt sätt att anmäla fel som uppstått i byggnader som administreras av Akademiska Hus.">
-  <meta name="theme-color" content="#6FBC4D">
-
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <meta name="mobile-web-app-capable" content="yes">
-
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="manifest" href="{{ asset('manifest.json') }}">
-
-  <link rel="shortcut icon" href="{{ asset('images/app-icon.png') }}">
-  <link rel="apple-touch-icon" href="{{ asset('images/app-icon.png') }}">
-
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-  <title>Campuskollen</title>
+  @include('layouts/head')
 </head>
 <body>
 
@@ -39,9 +20,6 @@ use Illuminate\Support\Facades\Auth;
 
   @include('partials/bot-navigation')
 
-  {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> --}}
     <script>
       if (document.getElementById('map')) {
         function initMap() {
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,20 @@
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Ett enklare och mer mobilt sätt att anmäla fel som uppstått i byggnader som administreras av Akademiska Hus.">
+  <meta name="theme-color" content="#6FBC4D">
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="mobile-web-app-capable" content="yes">
+
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="manifest" href="{{ asset('manifest.json') }}">
+
+  <link rel="shortcut icon" href="{{ asset('images/app-icon.png') }}">
+  <link rel="apple-touch-icon" href="{{ asset('images/app-icon.png') }}">
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <title>Campuskollen</title>
