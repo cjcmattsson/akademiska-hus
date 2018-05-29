8d912cb41afa674b55079f0dbf859f5fe32f9acd
cjcmattsson <cjcmattsson@gmail.com>
2018-05-29 14:27:56 +0200

    Added app-icon. IT WORKS

--- resources/views/layouts/campus.blade.php
+++ resources/views/layouts/campus.blade.php
@@ -2,12 +2,24 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Ett enklare och mer mobilt sätt att anmäla fel som uppstått i byggnader som administreras av Akademiska Hus.">
+  <meta name="theme-color" content="#6FBC4D">
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="mobile-web-app-capable" content="yes">
+
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  {{-- <link rel="manifest" href="{{ asset('manifest.json') }}"> --}}
+  <link rel="manifest" href="{{ asset('manifest.json') }}">
+
+  <link rel="shortcut icon" href="{{ asset('images/ahus-logo.png') }}">
+   <link rel="apple-touch-icon" href="{{ asset('images/app-icon.png') }}">
+
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
   <title>Campuskollen</title>
 </head>
 <body>
